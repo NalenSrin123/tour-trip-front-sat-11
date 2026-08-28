ed2345eb2bc3dd06b79f8bd641ee82bc0a17f372
ahmiiiyyy12 <nhorsreymi11@gmail.com>
2026-08-29 01:47:59 +0700

    create Manage Booking

---
+++ src/data/ManageBookingData.js
@@ -0,0 +1,38 @@
+export const ManageBookingData = [
+    {
+        id: 1,
+        customerName: "John Doe",
+        tourPackage: "City TourAngkor Wat Sunrise Explorer ",
+        bookingDate: "2023-08-15",
+        travelDate: "2023-09-01",
+        amount: 450.0,
+        status: "Confirmed",
+    },
+    {
+        id: 2,
+        customerName: "Jane Smith",
+        tourPackage: "Phnom Penh Cultural Tour",
+        bookingDate: "2023-08-20",
+        travelDate: "2023-09-15",
+        amount: 750.0,
+        status: "Pending",
+    },
+    {
+        id: 3,
+        customerName: "Bob Johnson",
+        tourPackage: "Siem Reap 3-Day Pass",
+        bookingDate: "2023-08-25",
+        travelDate: "2023-09-20",
+        amount: 600.0,
+        status: "Cancelled",
+    },
+    {
+        id: 4,
+        customerName: "Alice Williams",
+        tourPackage: "City TourAngkor Wat Sunrise Explorer",
+        bookingDate: "2023-08-15",
+        travelDate: "2023-09-01",
+        amount: 450.0,
+        status: "Completed",
+    }
+]
