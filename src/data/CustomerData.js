b625bece2e2a22565517f8c575c0843a158813d5
Ingingg222 <theavlying999@gmail.com>
2026-08-29 11:04:00 +0700

    Design page list customer

---
+++ src/data/CustomerData.js
@@ -0,0 +1,38 @@
+export const CustomerData = [
+    {
+        id : "CUS-9821",
+        name :"sarah Jenkins",
+        email : "sarah.j@example.com",
+        phone :"+855 12 345 678",
+        bookings : 4,
+        lastActivity : "Siem Reap Tour",
+        date : "Sep 30, 2023",
+        status: "Active",
+        image :"https://i.pinimg.com/1200x/0f/c6/c2/0fc6c2cc7e07259a62d59b9e9b87263d.jpg",
+    },
+
+     {
+        id : "CUS-7734",
+        name :"David Chen",
+        email : "d.cheng@global.net",
+        phone :"+65 9876 5432",
+        bookings : "1",
+        lastActivity : "Phnom Penh City",
+        date : "Sep 28 ,2023",
+        status: "Active",
+        image :"https://i.pinimg.com/1200x/01/4d/b5/014db573cb5ccf0dee67cf369f88ccf3.jpg",
+    },
+
+     {
+        id : "CUS-9012",
+        name :"Marcus Reed",
+        email : "m.reed_22@email.com",
+        phone :"+61 412 345 678",
+        bookings : "0",
+        lastActivity : "",
+        date : "Never booked",
+        status: "Blocked",
+        image :"null",
+    },
+];
+export default CustomerData;
