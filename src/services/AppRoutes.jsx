c019df929fb5e26366019dfc04da491a3f1b9658
Polina12 <pangpolina@gmail.com>
2026-09-04 19:57:41 +0700

    design page create destination

--- src/services/AppRoutes.jsx
+++ src/services/AppRoutes.jsx
@@ -2,14 +2,27 @@ import { Navigate, Routes, Route } from "react-router-dom";
 import DashboardOverview from "../pages/admin/dashboard/DashboardOverview";
 import { AdminLayout } from "../layouts/AdminLayout";
 import OtpVerifyPage from "../pages/public/auth/OtpVerifyPage";
+import CreateCustomer from "../pages/admin/customers/CreateCustomer";
+import CategoriesPage from "../pages/admin/categories/CategoriesPage";
+import ManageBooking from "../pages/admin/bookings/ManageBooking";
+import CreateDestination from "../pages/admin/destinations/CreateDestination";
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/admin/*" element={<AdminLayout />} />
-            <Route path="/admin/dashboardOverview" element={<DashboardOverview />} />
-            <Route path="/verify-otp" element={<OtpVerifyPage />} />
-            <Route path="*" element={<Navigate to="/verify-otp" replace />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<DashboardOverview />} />
+        <Route path="dashboardOverview" element={<DashboardOverview />} />
+        <Route path="masters" element={<CategoriesPage />} />
+        <Route path="customers" element={<CreateCustomer />} />
+        <Route path="bookings" element={<ManageBooking />} />
+        <Route path="destinations/create" element={<CreateDestination />} />
+      </Route>
+
+      <Route path="/verify-otp" element={<OtpVerifyPage />} />
+
+      <Route path="/" element={<Navigate to="/admin" replace />} />
+      <Route path="*" element={<Navigate to="/admin" replace />} />
+    </Routes>
+  );
+};
