3d2b24610fe51b4f2638fda5243b6fb51bc96dee
RofikinHasani <rofikinhasani1901@gmail.com>
2026-09-11 14:00:29 +0700

    Add trip detail content feature

--- src/services/AppRoutes.jsx
+++ src/services/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Navigate, Routes, Route } from "react-router-dom";
 import DashboardOverview from "../pages/admin/dashboard/DashboardOverview";
 import { AdminLayout } from "../layouts/AdminLayout";
 import OtpVerifyPage from "../pages/public/auth/OtpVerifyPage";
+import TripDetailPage from "../pages/public/trips/TripDetailPage";
 
 export const AppRoutes = () => {
     return (
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
             <Route path="/admin/*" element={<AdminLayout />} />
             <Route path="/admin/dashboardOverview" element={<DashboardOverview />} />
             <Route path="/verify-otp" element={<OtpVerifyPage />} />
+            <Route path="/trips/:id" element={<TripDetailPage />} />
             <Route path="*" element={<Navigate to="/verify-otp" replace />} />
         </Routes>
     )
