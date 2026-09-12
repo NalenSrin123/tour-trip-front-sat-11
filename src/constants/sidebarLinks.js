2d581b5fae774ca475bf90d80c05719e933f85d2
NalenSrin123 <srinnalen@gmail.com>
2026-09-12 11:45:35 +0700

    fix

--- src/constants/sidebarLinks.js
+++ src/constants/sidebarLinks.js
@@ -6,6 +6,8 @@ import {
   Star,
   Settings,
   LogOut,
+  CalendarDays,
+  Users
 } from "lucide-react";
 
 /**
@@ -29,11 +31,11 @@ export const sidebarLinks = [
     icon: CalendarCheck,
   },
   { label: "Manage Categories", path: "/admin/categories", icon: LayoutGrid },
-  // { label: "Tour Schedules", path: "/admin/tour-schedules", icon: CalendarDays },
+  { label: "Tour Schedules", path: "/admin/tour-schedules", icon: CalendarDays },
   // { label: "Manage Masters", path: "/admin/masters", icon: LayoutGrid },
   // { label: "Manage Bookings", path: "/admin/manageBooking", icon: CalendarCheck },
   // { label :"Manage Categories",path: "/admin/categories",icon: LayoutGrid,},
-  // { label: "Manage Customers", path: "/admin/customerList", icon: Users },
+  { label: "Manage Customers", path: "/admin/customerList", icon: Users },
   { label: "Reports", path: "/admin/reports", icon: BarChart3 },
   { label: "Reviews", path: "/admin/reviews", icon: Star },
 ];
