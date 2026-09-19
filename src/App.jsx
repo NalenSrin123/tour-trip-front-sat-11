3598583ba38884655e17136c3588e4f10a57279a
NalenSrin123 <srinnalen@gmail.com>
2026-09-19 11:30:42 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import TourTripHero from "./components/hero/TourTripHero";
-import ContactPage from "./pages/ContactPage";
+
 import { AppRoutes } from "./services/AppRoutes";
 
 export default function App() {
   return (
-    
-    <ContactPage/>
+    <div className="App">
+      <AppRoutes />
+    </div>
   );
 }
